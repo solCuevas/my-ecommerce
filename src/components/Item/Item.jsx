6328e748800a7051512ce248808dac310875c566
Sol Cuevas <cuevas.sol@hotmail.com>
2025-07-17 17:07:24 -0300

    Actualiza cards: quita descripción y reordena info

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,13 +1,30 @@
+
 import "./Item.css";
+
 
 const Item = ({ product }) => {
   return (
     <div className="product-card">
-      <img src={product.image} alt={product.name} />
+      <a
+        href={`/detail/${product.id}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={product.image} alt={product.name} className="product-image" />
+      </a>
+
       <div className="card-content">
         <h3>{product.name}</h3>
-        <p>{product.description}</p>
-        <span>${product.price}</span>
+        <span className="product-price">${product.price}</span>
+
+        <a
+          href={`/detail/${product.id}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="more-info-link"
+        >
+          Más información
+        </a>
       </div>
     </div>
   );
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
+import"./itemDetail.css"
 
-const ItemDetail = ({product = []}) => {
+const ItemDetail = ({ product = {} }) => {
+  if (!product || !product.name) {
+    return <p>Producto no encontrado</p>;
+  }
+
   return (
-    <div>
-       <img src={product.image} alt=""  />
-       <p>{product.name}</p>
-       <p> {product.description}</p>
-       <p>${product.price}</p>
-       </div>
-  )
-}
+    <div className="detalle-container">
+      <img
+        src={product.image}
+        alt={product.name}
+        className="detalle-imagen"
+      />
+      <h2 className="detalle-nombre">{product.name}</h2>
+      <p className="detalle-descripcion">{product.description}</p>
+      <p className="detalle-precio">Precio: ${product.price}</p>
+    </div>
+  );
+};
 
-export default ItemDetail
+export default ItemDetail;
